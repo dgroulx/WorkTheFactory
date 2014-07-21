1b3c4aad9cbce5b894eca769ac663e8a10876529
David Groulx <david@sandbendersoftware.com>
2014-07-21 08:53:07 -0400

    Initial Commit

---
+++ WorkTheFactory/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WorkTheFactory
+//
+//  Created by David Groulx on 7/21/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
