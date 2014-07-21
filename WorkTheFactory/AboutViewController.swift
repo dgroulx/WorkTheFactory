107b1a95f1e1512a4772ee869da68736d9ebf4ac
David Groulx <david@sandbendersoftware.com>
2014-07-21 16:38:39 -0400

    Enough of the about page laid out

---
+++ WorkTheFactory/AboutViewController.swift
@@ -0,0 +1,23 @@
+//
+//  AboutViewController.swift
+//  WorkTheFactory
+//
+//  Created by David Groulx on 7/21/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
