a044183175665ec2bca339cca72856d2ca7b7d99
David Groulx <david@sandbendersoftware.com>
2014-07-21 13:45:07 -0400

    Basic auto layout setup, UIAppearance proxy

--- WorkTheFactory/AppDelegate.swift
+++ WorkTheFactory/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
   func application(application: UIApplication!, didFinishLaunchingWithOptions launchOptions: NSDictionary!) -> Bool {
-    // Override point for customization after application launch.
+    UIButton.appearance().setTitleColor(UIColor.whiteColor(), forState: .Normal)
     return true
   }
 
---
+++ WorkTheFactory/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  WorkTheFactory
+//
+//  Created by David Groulx on 7/21/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
