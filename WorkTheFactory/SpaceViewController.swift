cdbd6c96cd2e30e4be0555db6f1d8c113e3741c5
David Groulx <david@sandbendersoftware.com>
2014-07-21 20:03:21 -0400

    Bare space view

---
+++ WorkTheFactory/SpaceViewController.swift
@@ -0,0 +1,23 @@
+//
+//  SpaceViewController.swift
+//  WorkTheFactory
+//
+//  Created by David Groulx on 7/21/14.
+//  Copyright (c) 2014 David Groulx. All rights reserved.
+//
+
+import UIKit
+
+class SpaceViewController: UIViewController {
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Do any additional setup after loading the view.
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+}
